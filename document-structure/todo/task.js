56515cf6bfbfa8a21bbde96545fb8e74657a78dd
Gatfanoff <Gatfanovm@mail.ru>
2023-05-25 13:08:06 +0500

    add document-structure

---
+++ document-structure/todo/task.js
@@ -0,0 +1,35 @@
+ 
+ const input = document.querySelector(".tasks__input");
+ const taskForm = document.getElementById("tasks__form");
+ const tasksList = document.querySelector(".tasks__list");
+ 
+ 
+ const createText = (text) => {
+    const taskDiv = document.createElement('div');
+    taskDiv.className = "task";
+
+    const taskTitle = document.createElement("div");
+    taskTitle.className = "task__title";
+    taskTitle.textContent = number;
+
+    const removeLink = document.createElement("a");
+    removeLink.href = "#";
+    removeLink.className = "task__remove";
+    removeLink.textContent = "×";
+
+    tasksList.appendChild(taskDiv);
+    taskDiv.appendChild(removeLink);
+    taskDiv.prepend(taskTitle);
+
+    removeLink.addEventListener('click', () => {
+        taskDiv.remove();
+    })
+ }
+
+ taskForm.addEventListener('submit', () => {
+    if(input.value.length > 0) {
+        createText(input.value);
+        input.value = "";
+    }
+ })
+
---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,24 @@
+const tooltips = document.querySelectorAll('.has-tooltip');
+const tooltipElement = document.querySelector('.tooltip');
+
+tooltips.forEach(tooltip => {
+  tooltip.addEventListener('click', (event) => {
+    event.preventDefault();
+    const tooltipText = tooltip.getAttribute('title');
+    
+    tooltipElement.textContent = tooltipText;
+    
+    const tooltipPosition = tooltip.getBoundingClientRect();
+    tooltipElement.style.top = `${tooltipPosition.bottom}px`;
+    tooltipElement.style.left = `${tooltipPosition.left}px`;
+
+    tooltipElement.classList.add('tooltip_active');
+  });
+});
+
+// Скрытие подсказки при клике вне элемента
+document.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('has-tooltip')) {
+    tooltipElement.classList.remove('tooltip_active');
+  }
+});
